cda56eb0039c727f7707302d2aa023386bf5459d
Aaron Hexamer <hexamer@comcast.net>
2015-11-10 18:39:59 -0600

    Initial changes toward issue 9.

--- FRA4PicoScope/Resource.h
+++ FRA4PicoScope/Resource.h
@@ -3,6 +3,7 @@
 // Used by PicoScopeFRA.rc
 //
 #define IDS_APP_TITLE                   100
+#define IDD_SETTINGS                    102
 #define IDD_MAIN                        200
 #define IDD_ABOUTBOX                    201
 #define IDD_SCOPESELECTORBOX            202
@@ -106,15 +107,32 @@
 #define IDR_MAINFRAME                   600
 #define IDB_PLOT_UNAVAILABLE            700
 #define IDB_SEPARATOR                   701
+#define IDC_RADIO1                      1000
+#define IDC_RADIO2                      1001
+#define IDC_RADIO3                      1002
+#define IDC_RADIO4                      1003
+#define IDC_EDIT1                       1004
+#define IDC_EDIT2                       1005
+#define IDC_BUTTON1                     1006
+#define IDC_BUTTON2                     1007
+#define IDC_CHECK1                      1008
+#define IDC_COMBO1                      1010
+#define IDC_EDIT3                       1011
+#define IDC_EDIT4                       1012
+#define IDC_EDIT5                       1013
+#define IDC_EDIT6                       1014
+#define IDC_EDIT7                       1015
+#define IDC_EDIT8                       1016
+#define IDC_EDIT9                       1017
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_RESOURCE_VALUE        104
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_CONTROL_VALUE         1016
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
