5c03991a9df0d3c2e476d6c528614cda595b506c
Aaron Hexamer <hexamer@comcast.net>
2017-07-16 15:52:58 -0500

    Release v0.6.1b

--- FRA4PicoScope/PicoScopeFRAApp.cpp
+++ FRA4PicoScope/PicoScopeFRAApp.cpp
@@ -56,7 +56,7 @@ double testPlotPhases[];
 double testPlotUnwrappedPhases[];
 #endif
 
-char* appVersionString = "0.6b";
+char* appVersionString = "0.6.1b";
 char* appNameString = "Frequency Response Analyzer for PicoScope";
 
 #define MAX_LOADSTRING 100
